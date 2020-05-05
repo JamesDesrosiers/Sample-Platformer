b5fa53c282536d72c1d6e6b9c83344ed8fa8f49d
James Desrosiers <JamesDavidDesrosiers@gmail.com>
2020-05-05 17:22:24 -0400

    Auto Tiling

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 10
-const MAX_SPEED = 200
+const MAX_SPEED = 300
 const JUMP_HEIGHT = -400
 
 const ACCELERATION = 10
@@ -13,7 +13,7 @@ var motion = Vector2()
 func _physics_process(delta):
 	#Gravity
 	motion.y += GRAVITY
-	
+
 	#X-axis movement
 	if Input.is_action_pressed("ui_right"):
 		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
@@ -25,11 +25,11 @@ func _physics_process(delta):
 		$Sprite.play("Run")
 	else:
 		if self.is_on_floor():
-			motion.x = lerp(motion.x, 0, 0.2)
+			motion.x = lerp(motion.x, 0, 0.10)
 		else:
 			motion.x = lerp(motion.x, 0, 0.01)
 		$Sprite.play("Idle")
-	
+
 	if self.is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
@@ -39,6 +39,6 @@ func _physics_process(delta):
 			$Sprite.play("Jump")
 		else:
 			$Sprite.play("Fall")
-	
+
 	motion = move_and_slide(motion, UP)
 	pass
